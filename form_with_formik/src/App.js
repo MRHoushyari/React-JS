0a504c4354d12eaecb7e52d1db293da4be85634e
mohammadreza <MR.Houshyari@Gmail.com>
2023-04-26 13:06:14 +0330

    New Project (Formik)

---
+++ form_with_formik/src/App.js
@@ -0,0 +1,10 @@
+import { Login } from "./components/loginform";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+export default App;
